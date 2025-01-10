5251611e0d95f1174abc2202b1c523ecb0bab432
juliangiebel <juliangiebel@live.de>
2025-01-10 18:27:27 +0100

    Address some reviews

--- Content.Server/IoC/ServerContentIoC.cs
+++ Content.Server/IoC/ServerContentIoC.cs
@@ -73,7 +73,7 @@ namespace Content.Server.IoC
             IoCManager.Register<PlayerRateLimitManager>();
             IoCManager.Register<SharedPlayerRateLimitManager, PlayerRateLimitManager>();
             IoCManager.Register<MappingManager>();
-            IoCManager.Register<SupportExchangeLogging, SupportExchangeLogging>();
+            IoCManager.Register<SupportExchangeLogging>();
         }
     }
 }
